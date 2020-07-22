322a85df72da6a6868e4cf1481872db1c09ba9bb
ramsha275 <33019590+ramsha275@users.noreply.github.com>
2020-07-22 18:37:32 +0500

    Model Loading Inside function

--- app.py
+++ app.py
@@ -1,25 +1,24 @@
-from flask import Flask , request , render_template
+from flask import Flask , render_template , request
 from werkzeug.utils import secure_filename
 from PIL import Image
 from matplotlib import pyplot
 import numpy as np
+import tensorflow as tf
+#from load_model import model
 import os
-from load_model import model
 
 app = Flask(__name__)
 app.debug = True
 
-
-
-@app.route('/')
+@app.route("/")
 def upload():
-    return render_template('upload.html')
+    return render_template('index.html')
 
 @app.route('/', methods = ['GET', 'POST'])
 def upload_file():
    # Create a directory in a known location to save files to.
-   uploads_dir = os.path.join(r'C:\Users\Ramsha\Desktop\MNIST FLASK APP', 'static')
-
+   uploads_dir = os.path.join(r'F:\PycharmProjects\MNIST APP', 'static')
+   model = tf.keras.models.load_model("mnist_ann_model.h5")
    if request.method == 'POST':
       f = request.files['file']
       f.save(os.path.join(uploads_dir , secure_filename(f.filename)))
@@ -31,10 +30,6 @@ def upload_file():
       pred = model.predict_classes(data.reshape(1,28,28))
 
       return render_template('prediction.html' , out = str(pred[0]) , im = f.filename)
-
-
-
-
 
 
 
